b474c830ded28a997d21bdf7c4cd170fab0600c0
Pa55er <tjrwns2715@naver.com>
2025-02-24 21:27:17 +0900

    🎉 프로젝트 setting 초기화

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": ["warn"],
+      "prettier/prettier": ["warn", { semi: false }],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
