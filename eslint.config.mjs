24a8447cea0b740dd8db57c24afd8d4d3f5852e8
Kim-HyounSuk <kkoor586@naver.com>
2025-03-22 02:06:16 +0900

    🔧 @typescript-eslint/no-unused-vars 룰 변경 (warn)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ const eslintConfig = [
             'no-unused-vars': ['warn'],
             'prettier/prettier': ['warn', { semi: false, endOfLine: 'auto' }],
             '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-unused-vars': ['warn'],
         },
     },
 ]
