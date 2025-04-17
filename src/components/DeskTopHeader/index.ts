532aedb404a507f77e7c0c3ac5e74953a52771bb
Pa55er <tjrwns2715@naver.com>
2025-04-17 18:44:48 +0900

    ✨ 데스크탑 전용 헤더 컴파운드 컴포넌트의 유저메뉴 컴포넌트 작성

--- src/components/DeskTopHeader/index.ts
+++ src/components/DeskTopHeader/index.ts
@@ -1,4 +1,5 @@
 import DesktopHeaderRoot from './DesktopHeaderRoot'
 import MenuItem from './MenuItem'
+import UserMenu from './UserMenu'
 
-export default Object.assign(DesktopHeaderRoot, { MenuItem })
+export default Object.assign(DesktopHeaderRoot, { MenuItem, UserMenu })
