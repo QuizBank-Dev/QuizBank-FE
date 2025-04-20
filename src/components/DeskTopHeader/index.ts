b39dbf10ba0fc40c99a87e6e9b9d3784176e63b7
Pa55er <tjrwns2715@naver.com>
2025-04-21 00:09:38 +0900

    🩹 폴더명 대소문자 수정을 위한 삭제

--- src/components/DeskTopHeader/index.ts
+++
@@ -1,5 +0,0 @@
-import DesktopHeaderRoot from './DesktopHeaderRoot'
-import MenuItem from './MenuItem'
-import UserMenu from './UserMenu'
-
-export default Object.assign(DesktopHeaderRoot, { MenuItem, UserMenu })
