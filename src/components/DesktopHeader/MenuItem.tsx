89acec2679ed5c4944ebe6d7643141abc560bbe9
Pa55er <tjrwns2715@naver.com>
2025-04-21 00:10:38 +0900

    🩹 폴더명 대소문자 수정을 위한 추가

---
+++ src/components/DesktopHeader/MenuItem.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import clsx from 'clsx'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+interface Props {
+    text: string
+    href: string
+}
+
+export default function MenuItem({ text, href }: Props) {
+    const pathname = usePathname()
+
+    return (
+        <div className="flex w-[130px] items-center justify-center">
+            <Link
+                href={href}
+                className={clsx(
+                    'text-pc-title-sm font-extra-bold',
+                    pathname.startsWith(href) && 'text-point-500',
+                )}
+            >
+                {text}
+            </Link>
+        </div>
+    )
+}
