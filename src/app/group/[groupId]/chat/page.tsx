40b5d9a72f20cfd55d5eae8ebe75356db3e0f0b3
Pa55er <tjrwns2715@naver.com>
2025-04-22 20:28:39 +0900

    ✨ 그룹 채팅 페이지 작성

---
+++ src/app/group/[groupId]/chat/page.tsx
@@ -0,0 +1,26 @@
+import MobileHeader from '@/components/MobileHeader'
+
+export default async function GroupChatPage({
+    params,
+}: Readonly<{
+    params: Promise<{ groupId: string }>
+}>) {
+    const { groupId } = await params
+
+    return (
+        <>
+            {/* 모바일 전용 헤더 */}
+            <MobileHeader title="그룹 채팅" backBtn />
+
+            {/* 컨탠츠 */}
+            <main className="flex w-full flex-1 flex-col items-center overflow-visible">
+                <div className="flex w-full flex-col gap-4 px-4 pb-4 md:px-0 md:pb-0">
+                    <h2 className="hidden text-pc-title-sm font-extra-bold text-point-900 md:block">
+                        그룹 채팅
+                    </h2>
+                    <GroupChat groupId={groupId} />
+                </div>
+            </main>
+        </>
+    )
+}
