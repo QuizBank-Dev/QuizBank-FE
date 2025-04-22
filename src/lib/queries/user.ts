bcccb0f866e9f92b23a6edf4e1b827d2d2c339f0
ppyom <ouob77@kakao.com>
2025-04-22 20:25:55 +0900

    ✨ 로그인한 사용자 정보를 가져오는 React Query 훅 구현

---
+++ src/lib/queries/user.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
+import { QueryKey } from '@/constants/common/queryKey'
+import { getCurrentUser } from '@/lib/api/user'
+
+type QueryType = Awaited<ReturnType<typeof getCurrentUser>>
+
+/**
+ * 현재 로그인한 유저를 가져오는 query
+ */
+export const useCurrentUser = () => {
+    return useQuery<QueryType>({
+        queryKey: QueryKey.user.DEFAULT,
+        queryFn: () => getCurrentUser(),
+        staleTime: 0,
+    })
+}
---
+++ src/types/user.d.ts
@@ -0,0 +1,22 @@
+export interface User {
+    _id: string
+    nickname: string
+    profileImg: string
+    introduce?: string
+    experience: number
+}
+
+/**
+ * 현재 로그인한 사용자
+ */
+export interface CurrentUser extends User {
+    category: string[]
+    isOAuthAccount: boolean
+}
+
+/**
+ * 다른 사용자
+ */
+export interface OtherUser extends User {
+    follower: string[]
+}
