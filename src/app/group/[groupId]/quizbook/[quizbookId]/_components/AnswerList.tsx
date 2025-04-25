82705b83037841c833bdf6bb7f7678ab3693c898
Pa55er <tjrwns2715@naver.com>
2025-04-26 02:00:35 +0900

    ✨ 그룹 선정 문제집 그룹원 답안 리스트 컴포넌트 작성

---
+++ src/app/group/[groupId]/quizbook/[quizbookId]/_components/AnswerList.tsx
@@ -0,0 +1,40 @@
+import { SolvedCard } from '@/components/study'
+
+const answerList = [
+    {
+        answer: 'test',
+        score: 10,
+        owner: {
+            _id: '1',
+            nickname: '쭈니1',
+            profileImg: '',
+        },
+    },
+    {
+        answer: 'test',
+        score: 10,
+        owner: {
+            _id: '2',
+            nickname: '쭈니2',
+            profileImg: '',
+        },
+    },
+]
+
+export default function AnswerList({ quizId }: { quizId: string }) {
+    return (
+        <div className="flex w-full flex-col gap-4 md:gap-8 md:pb-4">
+            {answerList.map((data) => (
+                <SolvedCard
+                    key={data.owner._id}
+                    user={{
+                        nickname: data.owner.nickname,
+                        profileUrl: data.owner.profileImg,
+                    }}
+                    answer={data.answer}
+                    score={data.score}
+                />
+            ))}
+        </div>
+    )
+}
--- src/app/group/[groupId]/quizbook/[quizbookId]/_components/MemberAnswer.tsx
+++ src/app/group/[groupId]/quizbook/[quizbookId]/_components/MemberAnswer.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx'
 import { Props } from './StudyStatus'
 import { QuestionCard } from '@/components/study'
 import { useState } from 'react'
+import AnswerList from './AnswerList'
 
 export default function MemberAnswer({
     quizList,
@@ -34,7 +35,7 @@ export default function MemberAnswer({
             <span className="hidden text-pc-body-md text-gray-600 md:block">
                 그룹원 답안
             </span>
-            <div className="flex w-full flex-col items-center gap-4 md:gap-8">
+            <div className="mx-4 flex flex-col items-center gap-4 md:mx-8 md:gap-8">
                 <QuestionCard
                     curIdx={pageNumber + 1}
                     totalIdx={quizList.length}
@@ -46,7 +47,7 @@ export default function MemberAnswer({
                     onNext={onNextHandler}
                 />
                 {showAnswers ? (
-                    <div></div>
+                    <AnswerList quizId={quizList[pageNumber]._id} />
                 ) : (
                     <span
                         className="cursor-pointer text-mobile-body-sm text-point-500 md:text-pc-body-sm"
