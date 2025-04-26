7fe01704311664a7d1aecb777d1db1e20be1caad
Pa55er <tjrwns2715@naver.com>
2025-04-27 01:13:50 +0900

    💄 문제집 상세 정보 section 퍼블리싱

--- src/app/quizbook/(no-layout)/[quizbookId]/(layout)/layout.tsx
+++ src/app/quizbook/(no-layout)/[quizbookId]/(layout)/layout.tsx
@@ -1,10 +1,14 @@
 import DesktopHeader from '@/components/DesktopHeader'
+import MobileHeader from '@/components/MobileHeader'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex h-screen flex-col items-center text-gray-900">
+        <div className="flex h-screen flex-col items-center font-semi-bold text-gray-900">
             {/* 데스크탑 전용 헤더 */}
             <DesktopHeader />
+
+            {/* 모바일 전용 헤더 */}
+            <MobileHeader title="그룹 정보" backBtn />
 
             {/* 컨탠츠 */}
             {children}
