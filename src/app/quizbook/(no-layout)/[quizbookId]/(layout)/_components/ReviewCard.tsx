530cdb8063ccca0db9b4368195b32001fab4e0ae
Pa55er <tjrwns2715@naver.com>
2025-04-28 06:56:31 +0900

    💄 문제집 후기 카드 컴포넌트 퍼블리싱

---
+++ src/app/quizbook/(no-layout)/[quizbookId]/(layout)/_components/ReviewCard.tsx
@@ -0,0 +1,133 @@
+'use client'
+
+import clsx from 'clsx'
+import StarFullIcon from '@/assets/svgs/star-full.svg'
+import StarHalfIcon from '@/assets/svgs/star-half.svg'
+import StarEmptyIcon from '@/assets/svgs/star-empty.svg'
+import OptionIcon from '@/assets/svgs/elipsis-v.svg'
+import EditIcon from '@/assets/svgs/edit.svg'
+import TrashIcon from '@/assets/svgs/trash.svg'
+import Link from 'next/link'
+import { ProfileImage } from '@/components'
+import { useState } from 'react'
+
+interface Props {
+    _id: string
+    score: number
+    content: string
+    author: { _id: string; nickname: string; profileImg: string }
+    updatedAt: string
+    myReview: boolean
+}
+
+export default function ReviewCard({
+    _id,
+    score,
+    content,
+    author,
+    updatedAt,
+    myReview,
+}: Props) {
+    const [isDropMenuOpen, setIsDropMenuOpen] = useState(false)
+
+    const handleDropMenu = () => {
+        setIsDropMenuOpen((prev) => !prev)
+    }
+
+    return (
+        <article
+            className={clsx(
+                'flex w-full flex-col rounded-lg border-b-1 border-gray-200 p-4 text-mobile-body-md md:text-pc-body-md',
+                myReview && 'bg-point-50',
+            )}
+        >
+            <div className="flex w-full items-center justify-between">
+                <div className="flex items-center gap-2 md:gap-4">
+                    <Link
+                        href={`/user/${_id}`}
+                        className="flex cursor-pointer items-center gap-2 md:gap-4"
+                    >
+                        <ProfileImage size={32} profileImg={``} />
+                        <span className="text-mobile-body-md font-semi-bold md:text-pc-body-md">
+                            {author.nickname}
+                        </span>
+                    </Link>
+                    <span className="text-mobile-caption font-regular text-gray-400 md:text-pc-caption">
+                        {updatedAt}
+                    </span>
+                </div>
+                <div className="relative">
+                    {myReview && (
+                        <OptionIcon
+                            className="size-5 cursor-pointer"
+                            onClick={handleDropMenu}
+                        />
+                    )}
+                    {isDropMenuOpen && (
+                        <div className="absolute right-0 top-5 flex flex-col items-start gap-2 rounded-lg bg-white p-4 shadow-point md:gap-4">
+                            <Link
+                                href={``}
+                                className="flex items-center gap-2 md:gap-4"
+                            >
+                                <EditIcon className="size-5" />
+                                <span className="whitespace-nowrap text-mobile-body-md md:text-pc-body-md">
+                                    수정
+                                </span>
+                            </Link>
+                            <Link
+                                href={``}
+                                className="flex items-center gap-2 md:gap-4"
+                            >
+                                <TrashIcon className="size-5" />
+                                <span className="whitespace-nowrap text-mobile-body-md md:text-pc-body-md">
+                                    삭제
+                                </span>
+                            </Link>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <div className="flex items-center gap-[10px] py-[2.5px]">
+                <div className="flex">
+                    {score === 0 ? (
+                        <StarEmptyIcon className="size-5 text-[#FFCC00]" />
+                    ) : score < 1 ? (
+                        <StarHalfIcon className="size-5 text-[#FFCC00]" />
+                    ) : (
+                        <StarFullIcon className="size-5 text-[#FFCC00]" />
+                    )}
+                    {score <= 1 ? (
+                        <StarEmptyIcon className="size-5 text-[#FFCC00]" />
+                    ) : score < 2 ? (
+                        <StarHalfIcon className="size-5 text-[#FFCC00]" />
+                    ) : (
+                        <StarFullIcon className="size-5 text-[#FFCC00]" />
+                    )}
+                    {score <= 2 ? (
+                        <StarEmptyIcon className="size-5 text-[#FFCC00]" />
+                    ) : score < 3 ? (
+                        <StarHalfIcon className="size-5 text-[#FFCC00]" />
+                    ) : (
+                        <StarFullIcon className="size-5 text-[#FFCC00]" />
+                    )}
+                    {score <= 3 ? (
+                        <StarEmptyIcon className="size-5 text-[#FFCC00]" />
+                    ) : score < 4 ? (
+                        <StarHalfIcon className="size-5 text-[#FFCC00]" />
+                    ) : (
+                        <StarFullIcon className="size-5 text-[#FFCC00]" />
+                    )}
+                    {score <= 4 ? (
+                        <StarEmptyIcon className="size-5 text-[#FFCC00]" />
+                    ) : score < 5 ? (
+                        <StarHalfIcon className="size-5 text-[#FFCC00]" />
+                    ) : (
+                        <StarFullIcon className="size-5 text-[#FFCC00]" />
+                    )}
+                </div>
+                <span className="font-semi-bold">{score}</span>
+            </div>
+            <p className="px-1 py-[10px] font-semi-bold">{content}</p>
+        </article>
+    )
+}
--- src/app/quizbook/(no-layout)/[quizbookId]/(layout)/_components/ReviewList.tsx
+++ src/app/quizbook/(no-layout)/[quizbookId]/(layout)/_components/ReviewList.tsx
@@ -3,6 +3,52 @@
 import StarFullIcon from '@/assets/svgs/star-full.svg'
 import StarHalfIcon from '@/assets/svgs/star-half.svg'
 import StarEmptyIcon from '@/assets/svgs/star-empty.svg'
+import ReviewCard from './ReviewCard'
+
+const reviewList = [
+    {
+        _id: '67fe3c0c277a68a1ac3f4aa4',
+        score: 2.5,
+        content: '쏘쏘',
+        quizbook: '67fdc5ac1e49a2871aeb6657',
+        author: {
+            _id: '67fe18f8bb486fc72e9a8004',
+            nickname: '쭈니0',
+            profileImg: '',
+        },
+        createdAt: '2025-04-15T10:59:24.969Z',
+        updatedAt: '2025-04-15T10:59:24.969Z',
+        __v: 0,
+    },
+    {
+        _id: '67fdc64a1e49a2871aeb668d',
+        score: 4,
+        content: '좋아용',
+        quizbook: '67fdc5ac1e49a2871aeb6657',
+        author: {
+            _id: '67e2e20e5872c849d5dd4b86',
+            nickname: '쭈니1',
+            profileImg: '',
+        },
+        createdAt: '2025-04-15T02:36:58.467Z',
+        updatedAt: '2025-04-15T02:36:58.467Z',
+        __v: 0,
+    },
+    {
+        _id: '67fdc64a1e49a2871aeb6688',
+        score: 0,
+        content: '싫어용',
+        quizbook: '67fdc5ac1e49a2871aeb6657',
+        author: {
+            _id: '67e2e20e5872c849d5dd4b87',
+            nickname: '쭈니2',
+            profileImg: '',
+        },
+        createdAt: '2025-04-15T02:36:58.467Z',
+        updatedAt: '2025-04-15T02:36:58.467Z',
+        __v: 0,
+    },
+]
 
 export default function ReviewList() {
     // 추후 데이터 패칭 추가
@@ -55,6 +101,17 @@ export default function ReviewList() {
                     {56}개의 후기
                 </span>
             </div>
+            {reviewList.map((data) => (
+                <ReviewCard
+                    key={data._id}
+                    _id={data._id}
+                    score={data.score}
+                    content={data.content}
+                    author={data.author}
+                    updatedAt={data.updatedAt}
+                    myReview={data._id === '67fe3c0c277a68a1ac3f4aa4'}
+                />
+            ))}
             <button className="btn-solid btn-mobile-lg w-full md:btn-pc-lg">
                 더보기
             </button>
