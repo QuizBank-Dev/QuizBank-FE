91e598b9703cdd027279d9215e1e523ca02fd269
ppyom <ouob77@kakao.com>
2025-04-28 13:51:58 +0900

    🐛 카드 클릭 이벤트가 타이틀에만 적용되도록 수정

--- src/components/QuizbookCard/QuizbookCardRoot.tsx
+++ src/components/QuizbookCard/QuizbookCardRoot.tsx
@@ -40,17 +40,19 @@ export default function QuizbookCardRoot({
         <QuizbookCardContext.Provider value={{ id, category, title, ...props }}>
             <div
                 className={clsx(
-                    'flex cursor-pointer flex-col items-start gap-2 rounded-lg bg-white p-4 shadow-point hover:bg-gray-100',
+                    'flex flex-col items-start gap-2 rounded-lg bg-white p-4 shadow-point hover:bg-gray-100',
                     className,
                 )}
-                onClick={handleQuizbookCardClick}
             >
                 <div className="flex w-full gap-1">
                     <div className="flex-1">
                         <div className="text-mobile-caption font-regular text-point-500 md:text-pc-caption">
                             {category}
                         </div>
-                        <div className="line-clamp-1 text-mobile-body-lg font-semi-bold md:text-pc-body-lg">
+                        <div
+                            className="line-clamp-1 cursor-pointer text-mobile-body-lg font-semi-bold md:text-pc-body-lg"
+                            onClick={handleQuizbookCardClick}
+                        >
                             {title}
                         </div>
                     </div>
