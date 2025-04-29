2952d5defe3513fb5904f1c9c74a0c182949b0dc
Kim-HyounSuk <kkoor586@naver.com>
2025-04-29 11:01:53 +0900

    💄 중복 CSS 제거

--- src/app/quizbook/(no-layout)/post/page.tsx
+++ src/app/quizbook/(no-layout)/post/page.tsx
@@ -2,7 +2,7 @@ import { PostQuizbookForm } from './_components'
 
 export default function Page() {
     return (
-        <div className="flex min-h-0 w-full max-w-[768px] flex-1 flex-col gap-[16px] overflow-hidden md:gap-[32px] md:rounded-lg md:bg-white md:p-[16px] md:p-[32px] md:shadow-point">
+        <div className="flex min-h-0 w-full max-w-[768px] flex-1 flex-col gap-[16px] overflow-hidden md:gap-[32px] md:rounded-lg md:bg-white md:p-[32px] md:shadow-point">
             {/* Desktop 제목 영역 */}
             <h2 className="hidden text-center font-extra-bold text-point-900 md:block md:text-pc-title-sm">
                 문제집 생성
