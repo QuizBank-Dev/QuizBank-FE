30edf473f54934b96eb50536d9ffa29a3ffd381e
Pa55er <tjrwns2715@naver.com>
2025-05-01 00:36:34 +0900

    💄 그룹장 위임 모달 퍼블리싱

---
+++ src/app/group/[groupId]/member/@modal/[modalType]/[userId]/page.tsx
@@ -0,0 +1,22 @@
+import ChangeOwnerModal from './_components/ChangeOwnerModal'
+
+export default async function ModalWrapper({
+    params,
+}: {
+    params: Promise<{ modalType: string }>
+}) {
+    const { modalType } = await params
+
+    switch (modalType) {
+        case 'change-owner':
+            return <ChangeOwnerModal />
+        // case 'delete-member':
+        //     return <DeleteMemberModal />
+        // case 'accept-apply':
+        //     return <AcceptApplyModal />
+        // case 'reject-apply':
+        //     return <RejectApplyModal />
+        default:
+            return null
+    }
+}
