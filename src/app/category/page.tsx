f4c37476b0ef5dd1dfbe0fa3f24310b3654cc16f
ppyom <ouob77@kakao.com>
2025-05-02 12:49:42 +0900

    💄 선호 카테고리 페이지 패딩 수정

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -2,7 +2,7 @@ import CategorySelect from './_components/CategorySelect'
 
 export default function Page() {
     return (
-        <div className="flex w-full max-w-[450px] flex-col items-center gap-8 p-12">
+        <div className="flex w-full max-w-[450px] flex-col items-center gap-8 p-8 md:p-12">
             <div className="text-center">
                 <h2 className="mb-2 text-mobile-title-sm font-extra-bold text-point-900 md:text-pc-title-sm">
                     선호하는 카테고리를 선택해주세요
