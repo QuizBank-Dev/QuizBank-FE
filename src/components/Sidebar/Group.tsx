6e931dc8e8a94788e2d3ae23c3059f9abcf78bf0
ppyom <ouob77@kakao.com>
2025-05-02 15:38:02 +0900

    ♻️ Sidebar.Group에 className을 입력받을 수 있도록 수정

--- src/components/Sidebar/Group.tsx
+++ src/components/Sidebar/Group.tsx
@@ -1,10 +1,18 @@
+import clsx from 'clsx'
+
 interface Prop {
     children: React.ReactNode
+    className?: string
 }
 
-export default function Group({ children }: Prop) {
+export default function Group({ children, className }: Prop) {
     return (
-        <div className="flex w-full flex-col overflow-hidden rounded-lg bg-white">
+        <div
+            className={clsx(
+                'flex w-full flex-col overflow-hidden rounded-lg bg-white',
+                className,
+            )}
+        >
             {children}
         </div>
     )
