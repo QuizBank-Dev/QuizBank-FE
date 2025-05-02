6b00c9afba905317256f0049706279e65374f85b
ppyom <ouob77@kakao.com>
2025-05-02 23:57:12 +0900

    💄 사용자 정보 메뉴 컴포넌트 퍼블리싱

---
+++ src/app/my-page/info/_components/MyInfoMenu.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useCurrentUser } from '@/hooks/queries'
+import Sidebar from '@/components/Sidebar'
+
+import BadgeSvg from '@/assets/svgs/badge.svg'
+import PasswordSvg from '@/assets/svgs/password.svg'
+import CategorySvg from '@/assets/svgs/category.svg'
+
+export default function MyInfoMenu() {
+    const { data: user } = useCurrentUser()
+
+    return (
+        <Sidebar gap={4}>
+            <Sidebar.Group>
+                <Sidebar.Item
+                    icon={<BadgeSvg className="size-5" />}
+                    text="보유 뱃지"
+                    href="#"
+                />
+            </Sidebar.Group>
+            <Sidebar.Group>
+                <Sidebar.Item
+                    icon={<PasswordSvg className="size-5" />}
+                    text="비밀번호 변경"
+                    href="#"
+                />
+                <Sidebar.Item
+                    icon={<CategorySvg className="size-5" />}
+                    text="선호 카테고리"
+                    subText={
+                        user?.category.join(', ') ||
+                        '선택된 카테고리가 존재하지 않습니다.'
+                    }
+                    href="/category"
+                />
+            </Sidebar.Group>
+        </Sidebar>
+    )
+}
--- src/app/my-page/info/page.tsx
+++ src/app/my-page/info/page.tsx
@@ -1,10 +1,12 @@
 import CommonPageLayout from '../_components/CommonPageLayout'
 import UserProfile from './_components/UserProfile'
+import MyInfoMenu from './_components/MyInfoMenu'
 
 export default function Page() {
     return (
         <CommonPageLayout title="내 정보">
             <UserProfile />
+            <MyInfoMenu />
         </CommonPageLayout>
     )
 }
