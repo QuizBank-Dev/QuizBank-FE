15af3e87c3101ca8d5be094e620a6b75cfcd7df8
ppyom <ouob77@kakao.com>
2025-05-03 14:37:19 +0900

    🩹 회원탈퇴 추가

--- src/app/my-page/info/page.tsx
+++ src/app/my-page/info/page.tsx
@@ -2,6 +2,7 @@ import CommonPageLayout from '../_components/CommonPageLayout'
 import MyPageSidebar from '../_components/MyPageSidebar'
 import UserProfile from './_components/UserProfile'
 import MyInfoMenu from './_components/MyInfoMenu'
+import Link from 'next/link'
 
 export default function Page() {
     return (
@@ -9,6 +10,14 @@ export default function Page() {
             <UserProfile />
             <MyInfoMenu />
             <MyPageSidebar className="!block md:!hidden" />
+            <div className="text-center">
+                <Link
+                    className="text-mobile-body-sm text-gray-500 underline md:text-pc-body-sm"
+                    href="#"
+                >
+                    회원탈퇴
+                </Link>
+            </div>
         </CommonPageLayout>
     )
 }
