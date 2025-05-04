7e826f4739cd34a9793292e0bed4000cf13af2ff
ppyom <ouob77@kakao.com>
2025-05-04 21:29:04 +0900

    💄 다른 사용자 페이지 구조 작성

---
+++ src/app/user/[userId]/page.tsx
@@ -0,0 +1,31 @@
+import { notFound } from 'next/navigation'
+import MobileHeader from '@/components/MobileHeader'
+
+const user = {
+    _id: '2',
+    nickname: 'example11',
+    profileImg: '',
+    introduce: '',
+    experience: 3000,
+    follower: ['1'],
+}
+
+export default function Page() {
+    if (!user) {
+        return notFound()
+    }
+
+    return (
+        <>
+            <MobileHeader title={user.nickname} backBtn />
+            <div className="flex w-full max-w-[1056px] flex-col items-start gap-4 p-4 md:flex-row md:pt-8">
+                <main className="flex w-full flex-1 flex-col gap-4">
+                    <h2 className="hidden text-mobile-title-sm font-extra-bold text-point-900 md:block md:text-pc-title-sm">
+                        <span className="text-point-500">{user.nickname}</span>
+                        님의 학습 정보
+                    </h2>
+                </main>
+            </div>
+        </>
+    )
+}
