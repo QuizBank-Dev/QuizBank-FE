9be2d91160ebbf0fdba9cea38026c0eae41f650f
Pa55er <tjrwns2715@naver.com>
2025-05-07 03:01:34 +0900

    🩹 StaleTime보다 높은 GcTime 부여

--- src/hooks/queries/user/useCurrentUser.ts
+++ src/hooks/queries/user/useCurrentUser.ts
@@ -12,6 +12,7 @@ export const useCurrentUser = () => {
         queryKey: QueryKey.user.DEFAULT,
         queryFn: () => getCurrentUser(),
         staleTime: 1000 * 60 * 10,
+        gcTime: 1000 * 60 * 60,
         retry: 0,
         meta: {
             ignoreGlobalError: true,
