547da175e7ae46c22bb3ba2450a027e1fc360280
Pa55er <tjrwns2715@naver.com>
2025-05-08 16:20:57 +0900

    ✨ 공통 axiosInstance 적용

--- src/lib/api/group/createGroup.ts
+++ src/lib/api/group/createGroup.ts
@@ -1,12 +1,10 @@
 import { CreateGroupFormData } from '@/app/group/new/_components/GroupCreateForm'
-import axios from 'axios'
+import axiosInstance from '../base'
 
 /**
  * 그룹을 생성하는 함수
  * @param data 그룹 생성에 필요한 그룹 이름, 그룹 소개 정보
  */
-export const createGroup = async (data: CreateGroupFormData) => {
-    return axios.post(`${process.env.NEXT_PUBLIC_API_URL}/v1/group`, data, {
-        withCredentials: true,
-    })
+export const createGroup = (data: CreateGroupFormData) => {
+    return axiosInstance.post('/v1/group', data)
 }
