b0b53f0ac916bf7fbd19820f0bbc410f306213ff
Kim-HyounSuk <kkoor586@naver.com>
2025-05-09 13:46:28 +0900

    ✨ 사용자 작성 답안 상태관리 전역 스토어 구현

---
+++ src/store/quizbook/answerStore.ts
@@ -0,0 +1,42 @@
+import { create, StoreApi, UseBoundStore } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface AnswerStore {
+    answerMap: Record<string, string>
+    setAnswer: (quizId: string, answer: string) => void
+    reset: () => void
+}
+
+type AnswerStoreType = UseBoundStore<StoreApi<AnswerStore>>
+
+const storeMap = new Map<string, AnswerStoreType>()
+
+const getAnswerStore = (quizbookId: string) => {
+    if (!storeMap.has(quizbookId)) {
+        const store = create(
+            persist<AnswerStore>(
+                (set) => ({
+                    answerMap: {},
+                    setAnswer: (quizId, answer) =>
+                        set((state) => ({
+                            answerMap: {
+                                ...state.answerMap,
+                                [quizId]: answer,
+                            },
+                        })),
+                    reset: () => set({ answerMap: {} }),
+                }),
+                {
+                    name: `quizbook-answer-list-${quizbookId}`,
+                },
+            ),
+        )
+        storeMap.set(quizbookId, store)
+
+        return store
+    }
+
+    return storeMap.get(quizbookId)!
+}
+
+export default getAnswerStore
