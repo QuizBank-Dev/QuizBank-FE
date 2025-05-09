7308a107659cfcfd627608ecccf9efa549b6a7d9
Pa55er <tjrwns2715@naver.com>
2025-05-10 01:43:58 +0900

    🔧 Mutation 전역 에러 핸들링 추가

--- src/lib/react-query/getQueryClient.ts
+++ src/lib/react-query/getQueryClient.ts
@@ -2,6 +2,7 @@ import {
     QueryClient,
     defaultShouldDehydrateQuery,
     QueryCache,
+    MutationCache,
 } from '@tanstack/react-query'
 import axios from 'axios'
 import { toast } from 'sonner'
@@ -23,6 +24,21 @@ const makeQueryClient = () => {
         queryCache: new QueryCache({
             onError: (error, query) => {
                 const meta = query?.meta as Record<string, boolean>
+                if (isClient && !meta?.ignoreGlobalError) {
+                    let message = '알 수 없는 에러가 발생했습니다.'
+                    if (axios.isAxiosError(error)) {
+                        // 서버에서 내려준 메시지 우선
+                        message = error.response?.data?.message || error.message
+                    } else if (error instanceof Error) {
+                        message = error.message
+                    }
+                    toast(message)
+                }
+            },
+        }),
+        mutationCache: new MutationCache({
+            onError: (error, _a, _b, mutation) => {
+                const meta = mutation?.meta as Record<string, boolean>
                 if (isClient && !meta?.ignoreGlobalError) {
                     let message = '알 수 없는 에러가 발생했습니다.'
                     if (axios.isAxiosError(error)) {
