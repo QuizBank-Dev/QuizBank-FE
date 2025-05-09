6c0d635a490a533886af5dc27a56f86b4d50ba8c
Pa55er <tjrwns2715@naver.com>
2025-05-09 18:14:00 +0900

    🔧 Query 전역 에러 핸들링 수정

--- src/lib/react-query/getQueryClient.ts
+++ src/lib/react-query/getQueryClient.ts
@@ -3,6 +3,7 @@ import {
     defaultShouldDehydrateQuery,
     QueryCache,
 } from '@tanstack/react-query'
+import axios from 'axios'
 import { toast } from 'sonner'
 
 let browserQueryClient: QueryClient | undefined = undefined
@@ -23,7 +24,14 @@ const makeQueryClient = () => {
             onError: (error, query) => {
                 const meta = query?.meta as Record<string, boolean>
                 if (isClient && !meta?.ignoreGlobalError) {
-                    toast(error.message)
+                    let message = '알 수 없는 에러가 발생했습니다.'
+                    if (axios.isAxiosError(error)) {
+                        // 서버에서 내려준 메시지 우선
+                        message = error.response?.data?.message || error.message
+                    } else if (error instanceof Error) {
+                        message = error.message
+                    }
+                    toast(message)
                 }
             },
         }),
