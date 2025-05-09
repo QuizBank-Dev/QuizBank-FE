d25442c5e454ea66ce043d3d85b69b8f3f891fd1
Kim-HyounSuk <kkoor586@naver.com>
2025-05-09 13:42:38 +0900

    🏷️ Quiz 타입 확장 (정답 포함)

--- src/types/quiz.d.ts
+++ src/types/quiz.d.ts
@@ -3,8 +3,8 @@ import { QUIZ_TYPE } from '@/constants/quiz'
 // Quiz > type 필드 타입
 export type QuizType = (typeof QUIZ_TYPE)[keyof typeof QUIZ_TYPE]
 
-// Quiz 타입
-export type Quiz =
+// AddedQuiz 타입
+export type PostQuiz =
     | { type: typeof QUIZ_TYPE.OX; question: string; answer: 'O' | 'X' }
     | {
           type: typeof QUIZ_TYPE.MULTIPLE
@@ -14,3 +14,16 @@ export type Quiz =
       }
     | { type: typeof QUIZ_TYPE.SHORT; question: string; answer: string }
     | { type: typeof QUIZ_TYPE.LONG; question: string; answer: string }
+
+// Quiz 타입 (정답 미포함)
+export interface Quiz {
+    _id: string
+    type: QuizType
+    question: string
+    optionList?: string[]
+}
+
+// Quiz 타입 (정답 포함)
+export interface QuizWithAnswer extends Quiz {
+    answer: string
+}
