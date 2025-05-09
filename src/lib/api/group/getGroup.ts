fc0f490f419da60c5506252308bccf9f25d2f06b
Pa55er <tjrwns2715@naver.com>
2025-05-09 18:12:43 +0900

    ✨ 그룹 정보 조회 query 작성

--- src/lib/api/group/getGroup.ts
+++ src/lib/api/group/getGroup.ts
@@ -1,9 +1,13 @@
+import { Group } from '@/types/group'
 import axiosInstance from '../base'
 
 /**
  * 그룹 정보를 조회하는 함수
  * @param groupId 정보를 조회하고자 하는 그룹의 ID
  */
-export const getGroup = (groupId: string) => {
-    return axiosInstance.get(`/v1/group/${groupId}`)
+export const getGroup = async (groupId: string) => {
+    const res = await axiosInstance.get<{ result: Group }>(
+        `/v1/group/${groupId}`,
+    )
+    return res.data.result
 }
