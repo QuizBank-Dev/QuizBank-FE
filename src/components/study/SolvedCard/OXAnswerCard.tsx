3ea5261e6ca404b922b4cd26757bc141015b0cc5
Kim-HyounSuk <kkoor586@naver.com>
2025-05-09 13:56:27 +0900

    ✨ SolvedCard 컴포넌트 문제 유형별 분기 처리

---
+++ src/components/study/SolvedCard/OXAnswerCard.tsx
@@ -0,0 +1,35 @@
+import clsx from 'clsx'
+
+interface Props {
+    answer: string
+    correct: string
+}
+
+export default function OXAnswerCard({ answer, correct }: Props) {
+    return (
+        <div className="flex w-full flex-1 items-center justify-center gap-[16px]">
+            {['O', 'X'].map((opt) => {
+                const isCorrect = correct === opt.toLowerCase()
+                const isWrong =
+                    opt.toLowerCase() === answer && answer !== correct
+
+                return (
+                    <div
+                        key={opt}
+                        className={clsx(
+                            'flex-1 rounded-lg border-2 text-center text-[64px] font-semi-bold md:text-[128px]',
+                            'hover:bg-gray-50 active:bg-gray-50',
+                            opt === 'O' ? 'text-point-200' : 'text-danger-300',
+                            {
+                                'border-point-500': isCorrect,
+                                'border-danger-300': isWrong,
+                            },
+                        )}
+                    >
+                        {opt}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
