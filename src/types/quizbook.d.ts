d6cd9e82f07a7ab594bf77b060999bd4bbf3e816
Kim-HyounSuk <kkoor586@naver.com>
2025-05-10 22:49:24 +0900

    🏷️ Quizbook 타입 지정

--- src/types/quizbook.d.ts
+++ src/types/quizbook.d.ts
@@ -9,13 +9,15 @@ export interface Quizbook<T = unknown> {
     description: string
     category: QuizbookCategoryType
     solvedCount: number
-    solvedRate: number
+    solvedScore: number
     reviewCount: number
-    reviewRate: number
+    reviewScore: number
+    reviewRating: number
     quizList: T[]
     // TODO User 타입으로 분리 필요
-    author: {
-        profileImg?: string
-        nickname: string
-    }
+    author: Author
+    isLiked: boolean
+    isStudied: boolean
+    createdAt: string
+    updatedAt: string
 }
