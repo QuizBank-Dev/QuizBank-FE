c95090bf6c7bd4d11150251cf6f6361220ed3145
Pa55er <tjrwns2715@naver.com>
2025-05-11 01:44:34 +0900

    💄 그룹원 정보 카드 ui 수정

--- src/app/group/[groupId]/member/@main/_components/GroupMember.tsx
+++ src/app/group/[groupId]/member/@main/_components/GroupMember.tsx
@@ -9,7 +9,7 @@ interface Props {
         profileImg: string
         email: string
     }
-    status: '방장' | '그룹원' | '가입 요청중'
+    status: '방장' | '그룹원' | '가입 요청'
     myId: string
     isOwner: boolean
 }
@@ -21,14 +21,31 @@ export default function GroupMember({ data, status, myId, isOwner }: Props) {
         <>
             <article className="hidden w-full items-center gap-5 rounded-lg bg-white p-4 text-pc-body-md font-semi-bold shadow-point md:flex">
                 <div className="cursor-pointer">
-                    <ProfileImage size={32} profileImg={data.profileImg} />
+                    <Link
+                        href={
+                            data._id === myId
+                                ? '/my-page/info'
+                                : `/user/${data._id}`
+                        }
+                    >
+                        <ProfileImage size={32} profileImg={data.profileImg} />
+                    </Link>
                 </div>
                 <div className="block w-0 flex-[2] break-words">
-                    <span className="cursor-pointer">{data.nickname}</span>
+                    <Link
+                        href={
+                            data._id === myId
+                                ? '/my-page/info'
+                                : `/user/${data._id}`
+                        }
+                        className="cursor-pointer"
+                    >
+                        {data.nickname}
+                    </Link>
                 </div>
                 <div className="h-8 border-1 border-white" />
                 <span className="block w-0 flex-[4] break-words">
-                    {data.email}
+                    {data._id}
                 </span>
                 <div className="h-8 border-1 border-white" />
                 <div className="flex w-full flex-[7] items-center justify-between">
@@ -58,7 +75,7 @@ export default function GroupMember({ data, status, myId, isOwner }: Props) {
                                 </Link>
                             </>
                         )}
-                        {isOwner && status === '가입 요청중' && (
+                        {isOwner && status === '가입 요청' && (
                             <>
                                 <Link
                                     className="rounded-lg bg-point-500 px-3 py-2 text-white"
@@ -78,11 +95,23 @@ export default function GroupMember({ data, status, myId, isOwner }: Props) {
                 </div>
             </article>
             <article className="flex w-full flex-col items-start gap-2 rounded-lg bg-white p-4 font-semi-bold shadow-point md:hidden">
-                <div className="flex cursor-pointer items-center gap-2">
+                <Link
+                    href={
+                        data._id === myId
+                            ? '/my-page/info'
+                            : `/user/${data._id}`
+                    }
+                    className="flex cursor-pointer items-center gap-2"
+                >
                     <ProfileImage size={32} profileImg={data.profileImg} />
                     <span className="text-mobile-body-md">{data.nickname}</span>
+                </Link>
+                <div className="flex items-center gap-[10px]">
+                    <span className="text-mobile-body-lg text-point-500">
+                        ID
+                    </span>
+                    <span className="text-mobile-body-md">{data._id}</span>
                 </div>
-                <span className="text-mobile-body-md">{data.email}</span>
                 <div className="flex w-full items-center justify-between">
                     <div className="flex items-center gap-[10px]">
                         <span className="text-mobile-body-lg text-point-500">
@@ -115,7 +144,7 @@ export default function GroupMember({ data, status, myId, isOwner }: Props) {
                                 </Link>
                             </>
                         )}
-                        {isOwner && status === '가입 요청중' && (
+                        {isOwner && status === '가입 요청' && (
                             <>
                                 <Link
                                     className="rounded-lg bg-point-500 px-3 py-2 text-white"
