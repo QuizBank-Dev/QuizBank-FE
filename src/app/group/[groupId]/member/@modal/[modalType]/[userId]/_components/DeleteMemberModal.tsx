2b51eeef5ba76e3e1374eaded1abce6d205e42a2
Pa55er <tjrwns2715@naver.com>
2025-05-11 14:29:14 +0900

    ✨ 그룹원 강퇴 모달 API 연동

--- src/app/group/[groupId]/member/@modal/[modalType]/[userId]/_components/DeleteMemberModal.tsx
+++ src/app/group/[groupId]/member/@modal/[modalType]/[userId]/_components/DeleteMemberModal.tsx
@@ -1,21 +1,19 @@
 'use client'
 
 import { LoopAnimation, Modal } from '@/components'
+import { useDeleteMember } from '@/hooks/mutations'
 import clsx from 'clsx'
 import { useParams } from 'next/navigation'
-import { useState } from 'react'
 
 export default function DeleteMemberModal() {
-    const [isLoading, setIsLoading] = useState(false)
-    const params = useParams()
+    const { groupId, userId } = useParams()
+    const { mutate, isPending } = useDeleteMember(
+        groupId as string,
+        userId as string,
+    )
 
     const handleClick = () => {
-        // 추후 로직 수정
-        setIsLoading(true)
-        setTimeout(() => {
-            console.log(1)
-            setIsLoading(false)
-        }, 2000)
+        mutate()
     }
 
     return (
@@ -30,15 +28,15 @@ export default function DeleteMemberModal() {
                     <span>강퇴하시겠습니까?</span>
                 </div>
                 <button
-                    disabled={isLoading}
+                    disabled={isPending}
                     className={clsx(
                         'btn-solid btn-mobile-md bg-danger-300 md:btn-pc-md',
-                        isLoading && 'btn-loading',
+                        isPending && 'btn-loading',
                     )}
                     onClick={handleClick}
                 >
-                    {isLoading && <LoopAnimation />}
-                    {isLoading ? 'Loading...' : '강퇴'}
+                    {isPending && <LoopAnimation />}
+                    {isPending ? 'Loading...' : '강퇴'}
                 </button>
             </div>
         </Modal>
