066bc687cb10e9dcea9aea67062c7ccde1c97c94
Pa55er <tjrwns2715@naver.com>
2025-05-11 13:58:15 +0900

    ✨ 그룹원을 강퇴하는 mutation 작성

---
+++ src/hooks/mutations/group/useDeleteMember.ts
@@ -0,0 +1,22 @@
+import { deleteMember } from '@/lib/api'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
+import { toast } from 'sonner'
+
+/**
+ * 그룹원을 강퇴하는 mutation
+ */
+export const useDeleteMember = (groupId: string, memberId: string) => {
+    const queryClient = useQueryClient()
+    const router = useRouter()
+
+    return useMutation({
+        mutationFn: () => deleteMember(groupId, memberId),
+        retry: 0,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['group', groupId] })
+            router.back()
+            toast('강퇴가 완료되었습니다!')
+        },
+    })
+}
