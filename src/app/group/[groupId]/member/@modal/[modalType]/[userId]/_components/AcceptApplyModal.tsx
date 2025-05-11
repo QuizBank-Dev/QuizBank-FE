0a9217d007b4296ed9e49cc9738836220fe7e291
Pa55er <tjrwns2715@naver.com>
2025-05-11 14:51:29 +0900

    ✨ 그룹 가입 신청 수락 모달 API 연동

--- src/app/group/[groupId]/member/@modal/[modalType]/[userId]/_components/AcceptApplyModal.tsx
+++ src/app/group/[groupId]/member/@modal/[modalType]/[userId]/_components/AcceptApplyModal.tsx
@@ -1,21 +1,19 @@
 'use client'
 
 import { LoopAnimation, Modal } from '@/components'
+import { usePatchApplyResponse } from '@/hooks/mutations'
 import clsx from 'clsx'
 import { useParams } from 'next/navigation'
-import { useState } from 'react'
 
 export default function AcceptApplyModal() {
-    const [isLoading, setIsLoading] = useState(false)
-    const params = useParams()
+    const { groupId, userId } = useParams()
+    const { mutate, isPending } = usePatchApplyResponse(
+        groupId as string,
+        userId as string,
+    )
 
     const handleClick = () => {
-        // 추후 로직 수정
-        setIsLoading(true)
-        setTimeout(() => {
-            console.log(1)
-            setIsLoading(false)
-        }, 2000)
+        mutate(true)
     }
 
     return (
@@ -30,15 +28,15 @@ export default function AcceptApplyModal() {
                     <span>수락하시겠습니까?</span>
                 </div>
                 <button
-                    disabled={isLoading}
+                    disabled={isPending}
                     className={clsx(
                         'btn-solid btn-mobile-md md:btn-pc-md',
-                        isLoading && 'btn-loading',
+                        isPending && 'btn-loading',
                     )}
                     onClick={handleClick}
                 >
-                    {isLoading && <LoopAnimation />}
-                    {isLoading ? 'Loading...' : '수락'}
+                    {isPending && <LoopAnimation />}
+                    {isPending ? 'Loading...' : '수락'}
                 </button>
             </div>
         </Modal>
