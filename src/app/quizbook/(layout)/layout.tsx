de3a7c1d19f2aa1a685bc76479c4bf1760b65363
ppyom <ouob77@kakao.com>
2025-05-12 11:37:42 +0900

    💄 페이지 레이아웃 구조 작성

--- src/app/quizbook/(layout)/layout.tsx
+++ src/app/quizbook/(layout)/layout.tsx
@@ -1,8 +1,24 @@
+import DesktopHeader from '@/components/DesktopHeader'
+import MobileBottomNav from '@/components/MobileBottomNav'
+import MobileHeader from '@/components/MobileHeader'
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <>
-            <div>문제집 리스트 레이아웃</div>
-            <main>{children}</main>
-        </>
+        <div className="no-scrollbar flex h-full flex-col items-center overflow-auto bg-point-50 text-gray-900">
+            {/* 데스크탑 전용 헤더 */}
+            <DesktopHeader />
+
+            {/* 모바일 전용 헤더 */}
+            <MobileHeader title="문제집">
+                <MobileHeader.UserMenu />
+            </MobileHeader>
+
+            <main className="no-scrollbar flex w-full flex-1 flex-col items-center overflow-auto">
+                {children}
+            </main>
+
+            {/* 모바일 전용 바텀 Nav */}
+            <MobileBottomNav />
+        </div>
     )
 }
--- src/app/quizbook/(layout)/page.tsx
+++ src/app/quizbook/(layout)/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-    return <div>문제집 리스트 페이지</div>
+    return (
+        <div className="flex w-full max-w-[1056px] flex-col gap-4 px-4 py-4 md:py-8">
+            문제집 리스트 페이지
+        </div>
+    )
 }
