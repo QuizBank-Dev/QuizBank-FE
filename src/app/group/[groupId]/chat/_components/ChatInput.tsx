3c2da30c453876a076af2fc818743428bf21df9a
Pa55er <tjrwns2715@naver.com>
2025-05-13 05:00:10 +0900

    ✨ 메세지 전송 로직 구현

--- src/app/group/[groupId]/chat/_components/ChatInput.tsx
+++ src/app/group/[groupId]/chat/_components/ChatInput.tsx
@@ -1,15 +1,54 @@
 'use client'
 
 import SendIcon from '@/assets/svgs/send.svg'
+import { useGroupQuery } from '@/hooks/queries/group'
+import { useSocketStore } from '@/store/group'
+import { useParams } from 'next/navigation'
+import { useRef, useState } from 'react'
 
 export default function ChatInput() {
+    const { groupId } = useParams()
+    const { data } = useGroupQuery(groupId as string)
+    const socket = useSocketStore((state) => state.socket)
+    const [input, setInput] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    // 메시지 전송 함수
+    const sendMessage = () => {
+        if (!socket || !input.trim() || !data?.chatRoom) return
+        socket.emit('send_chat', {
+            chatRoomId: data.chatRoom,
+            content: input.trim(),
+        })
+        setInput('')
+        // input 포커스 유지
+        inputRef.current?.focus()
+    }
+
+    // 엔터 입력 처리
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter' && input.trim().length > 0) {
+            e.preventDefault()
+            sendMessage()
+        }
+    }
+
     return (
         <div className="relative w-full">
             <input
+                ref={inputRef}
                 type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={handleKeyDown}
                 className="input-solid input-mobile w-full pr-10 font-regular text-gray-900 md:input-pc"
             />
-            <SendIcon className="absolute right-3 top-1/2 size-5 -translate-y-1/2 cursor-pointer text-gray-400 md:size-6" />
+            <SendIcon
+                onClick={() => {
+                    if (input.trim().length > 0) sendMessage()
+                }}
+                className="absolute right-3 top-1/2 size-5 -translate-y-1/2 cursor-pointer text-gray-400 md:size-6"
+            />
         </div>
     )
 }
