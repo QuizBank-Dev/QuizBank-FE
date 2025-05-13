3bc3b76d0f11f6763af397d5135c666fd61d92cc
Kim-HyounSuk <kkoor586@naver.com>
2025-05-13 18:59:30 +0900

    🏷️ 문제집 정보 조회(메타, 유저 플래그, 통계) 타입 정의

--- src/types/quizbook.d.ts
+++ src/types/quizbook.d.ts
@@ -1,9 +1,13 @@
 import { QUIZBOOK_CATEGORY } from '@/constants/quizbook'
 import { Author } from '@/types/user'
+import { Quiz } from './quiz'
 
 export type QuizbookCategoryType =
     (typeof QUIZBOOK_CATEGORY)[keyof typeof QUIZBOOK_CATEGORY]
 
+/**
+ * 문제집 전체 타입
+ */
 export interface Quizbook<T = unknown> {
     _id: string
     title: string
@@ -11,6 +15,7 @@ export interface Quizbook<T = unknown> {
     category: QuizbookCategoryType
     solvedCount: number
     solvedScore: number
+    totalScore: number
     reviewCount: number
     reviewScore: number
     reviewRating: number
@@ -19,6 +24,43 @@ export interface Quizbook<T = unknown> {
     author: Author
     isLiked: boolean
     isStudied: boolean
+    createdAt: string
+    updatedAt: string
+}
+
+/**
+ * 문제집의 메타 정보 전용 타입
+ */
+export interface QuizbookMeta {
+    _id: string
+    title: string
+    description: string
+    category: QuizbookCategoryType
+    quizList: Quiz[]
+    totalScore: number
+    author: Author
+}
+
+/**
+ * 문제집의 유저 플래그 전용 타입
+ */
+export interface QuizbookUserFlags {
+    _id: string
+    isLiked: boolean
+    isStudied: boolean
+}
+
+/**
+ * 문제집의 통계 정보 전용 타입
+ */
+export interface QuizbookStates {
+    _id: string
+    solvedCount: number
+    solvedScore: number
+    totalScore: number
+    reviewCount: number
+    reviewScore: number
+    reviewRating: number
     createdAt: string
     updatedAt: string
 }
