e293fc8c4f9ad2a79c262fb042acd1176b82bd88
Kim-HyounSuk <kkoor586@naver.com>
2025-05-13 20:06:52 +0900

    📝 JSDoc 설명 추가

--- src/lib/api/quizbook/getQuizbookUserFlags.ts
+++ src/lib/api/quizbook/getQuizbookUserFlags.ts
@@ -2,6 +2,11 @@ import { Response } from '@/types/base'
 import axiosInstance from '../base'
 import { QuizbookUserFlags } from '@/types/quizbook'
 
+/**
+ * 문제집 유저 플래그 조회
+ * @param quizbookId 조회할 문제집의 ObjectId
+ * @returns 문제집의 유저 플래그 정보(QuizbookUserFlags 타입)
+ */
 export const getQuizbookUserFlags = async (quizbookId: string) => {
     const res = await axiosInstance.get<Response<QuizbookUserFlags>>(
         `/quizbook/${quizbookId}/user-flags`,
