f2ddcf275443380e703f0e035039e10c2dd85278
Kim-HyounSuk <kkoor586@naver.com>
2025-05-13 20:11:11 +0900

    📝 JSDoc 설명 추가

--- src/lib/api/quizbook/postQuizbook.ts
+++ src/lib/api/quizbook/postQuizbook.ts
@@ -2,6 +2,11 @@ import { PostQuizbookFormData } from '@/types/schemas/quizbook'
 import axiosInstance from '../base'
 import { EmptyResponse } from '@/types/base'
 
+/**
+ * 문제집 생성
+ * @param body 생성할 문제집의 FormData
+ * @returns 빈 응답 반환
+ */
 export const postQuizbook = async (body: PostQuizbookFormData) => {
     const res = await axiosInstance.post<EmptyResponse>('/quizbook', body)
 
