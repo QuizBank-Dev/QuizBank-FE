09cb51bebb862f02f231a1672f7e6856f3af4d3a
Kim-HyounSuk <kkoor586@naver.com>
2025-05-14 16:57:51 +0900

    ✨ 문제집 통계 정보 관련 쿼리 훅 작성

---
+++ src/hooks/queries/quizbook/useQuizbookStates.ts
@@ -0,0 +1,17 @@
+import { GcTime } from '@/constants/common/gcTime'
+import { QueryKey } from '@/constants/common/queryKey'
+import { StaleTime } from '@/constants/common/staleTime'
+import { getQuizbookStates } from '@/lib/api/quizbook'
+import { useSuspenseQuery } from '@tanstack/react-query'
+
+export const useQuizbookStates = (quizbookId: string) =>
+    useSuspenseQuery({
+        queryKey: [QueryKey.quizbook.STATES, quizbookId],
+        queryFn: () => getQuizbookStates(quizbookId),
+        staleTime: StaleTime.HALF,
+        gcTime: GcTime.HOUR,
+        retry: 0,
+        meta: {
+            ignoreGlobalError: true,
+        },
+    })
