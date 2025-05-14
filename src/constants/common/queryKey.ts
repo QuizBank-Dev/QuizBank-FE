0eae76d8a275db92d0a52970e219880e97ec9499
Kim-HyounSuk <kkoor586@naver.com>
2025-05-14 16:59:51 +0900

    🔧 쿼리 관련 const 추가

--- src/constants/common/queryKey.ts
+++ src/constants/common/queryKey.ts
@@ -2,4 +2,9 @@ export const QueryKey = {
     user: {
         DEFAULT: ['user'],
     },
+    quizbook: {
+        STATES: 'quizbook-states',
+        META: 'quizbook-meta',
+        USERFLAGS: 'quizbook-user-flags',
+    },
 } as const
--- src/constants/common/staleTime.ts
+++ src/constants/common/staleTime.ts
@@ -1,3 +1,4 @@
 export const StaleTime = {
     DEFAULT: 1000 * 60 * 60, // 1시간
+    HALF: 1000 * 60 * 30, // 30분
 }
