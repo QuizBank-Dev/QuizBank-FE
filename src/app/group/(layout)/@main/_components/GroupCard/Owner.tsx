3d6c5381b7433b6480bf9f7e53427d1da13c83fd
Pa55er <tjrwns2715@naver.com>
2025-05-15 03:41:53 +0900

    ✨ 그룹 카드 프로필 ui Link 태그로 변경

--- src/app/group/(layout)/@main/_components/GroupCard/Owner.tsx
+++ src/app/group/(layout)/@main/_components/GroupCard/Owner.tsx
@@ -1,19 +1,29 @@
 import { ProfileImage } from '@/components'
 import { useGroupCardContext } from './GroupCardContext'
+import Link from 'next/link'
+import { useCurrentUser } from '@/hooks/queries/user'
 
 export default function Owner() {
     const { admin } = useGroupCardContext()
+    const { data: userData } = useCurrentUser()
 
-    if (!admin) {
+    if (!admin || !userData) {
         return null
     }
 
     return (
-        <div className="flex cursor-pointer items-center gap-2 md:gap-4">
+        <Link
+            className="flex cursor-pointer items-center gap-2 md:gap-4"
+            href={
+                admin._id === userData?._id
+                    ? '/my-page/info'
+                    : `/user/${admin._id}`
+            }
+        >
             <ProfileImage size={32} profileImg={admin.profileImg} />
             <span className="text-mobile-body-sm font-semi-bold md:text-pc-body-sm">
                 {admin.nickname}
             </span>
-        </div>
+        </Link>
     )
 }
