027e91e7f53bee065e2eafba6febc75ee7f10f1c
ppyom <ouob77@kakao.com>
2025-05-15 10:47:18 +0900

    ♻️ SignupForm Schema 분리

--- src/app/(auth)/signup/_components/SignupForm.tsx
+++ src/app/(auth)/signup/_components/SignupForm.tsx
@@ -3,51 +3,24 @@
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
-import * as z from 'zod'
 import { toast } from 'sonner'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { CustomInput } from '@/components'
 import { useEmailVerification } from '@/hooks/useEmailVerification'
+import { SignupFormData, signupSchema } from '@/types/schemas/auth'
 import LoadingButton from '../../_components/LoadingButton'
-
-const schema = z
-    .object({
-        email: z.string().email('이메일 형식으로 입력해주세요.'),
-        code: z.string(),
-        password: z
-            .string()
-            .nonempty('필수 입력되어야하는 항목입니다.')
-            .regex(
-                /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d`~!@#$%^&*()\-_=+\\|/?,.<>;:'"[\]{}]+$/,
-                '비밀번호는 8자리 이상, 영문과 숫자를 1가지 이상 조합해주세요.',
-            )
-            .min(
-                8,
-                '비밀번호는 8자리 이상, 영문과 숫자를 1가지 이상 조합해주세요.',
-            ),
-        confirmPassword: z
-            .string()
-            .nonempty('필수로 입력되어야하는 항목입니다.'),
-        nickname: z.string().nonempty('필수로 입력되어야하는 항목입니다.'),
-    })
-    .refine((data) => data.password === data.confirmPassword, {
-        message: '비밀번호가 일치하지 않습니다.',
-        path: ['confirmPassword'],
-    })
-
-type FormData = z.infer<typeof schema>
 
 export default function SignupForm() {
     const router = useRouter()
     const [isLoading, setIsLoading] = useState(false)
-    const methods = useForm<FormData>({
-        resolver: zodResolver(schema),
+    const methods = useForm<SignupFormData>({
+        resolver: zodResolver(signupSchema),
         mode: 'onChange',
     })
     const { isVerified, isVerifying, isSending, timer, sendCode, verifyCode } =
         useEmailVerification()
 
-    const handleFormSubmit = async (data: FormData) => {
+    const handleFormSubmit = async (data: SignupFormData) => {
         setIsLoading(true)
         // TODO 회원가입 API 호출
         const result = await new Promise<string>((resolve) =>
