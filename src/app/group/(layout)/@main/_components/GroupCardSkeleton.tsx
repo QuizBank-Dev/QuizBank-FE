1c6865ceb0d6873da4edf0e72f0b93b89a9d54ff
Pa55er <tjrwns2715@naver.com>
2025-05-15 09:04:56 +0900

    💄 그룹 카드 스켈레톤 ui 추가

---
+++ src/app/group/(layout)/@main/_components/GroupCardSkeleton.tsx
@@ -0,0 +1,14 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function GroupCardSkeleton() {
+    return (
+        <article className="flex flex-col items-start gap-2 rounded-lg bg-white px-4 py-4 shadow-point md:px-8">
+            <div className="flex w-full items-center justify-between">
+                <Skeleton className="h-8 w-[100px] rounded-lg md:h-9 md:w-[140px]" />
+                <Skeleton className="h-8 w-[100px] rounded-lg md:h-9 md:w-[140px]" />
+            </div>
+            <Skeleton className="h-[18px] w-full rounded-lg md:h-6" />
+            <Skeleton className="h-8 w-[85px] rounded-lg" />
+        </article>
+    )
+}
