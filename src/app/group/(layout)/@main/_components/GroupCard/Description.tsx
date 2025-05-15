96f882dae9fc4b51570a7c29c76574aebec90b42
Pa55er <tjrwns2715@naver.com>
2025-05-15 12:14:28 +0900

    💄 그룹 카드 소개란 줄바꿈 적용

--- src/app/group/(layout)/@main/_components/GroupCard/Description.tsx
+++ src/app/group/(layout)/@main/_components/GroupCard/Description.tsx
@@ -8,7 +8,7 @@ export default function Description() {
     }
 
     return (
-        <span className="w-full whitespace-normal break-words text-mobile-body-md font-regular text-gray-500 md:text-pc-body-md">
+        <span className="w-full whitespace-pre-line break-words text-mobile-body-md font-regular text-gray-500 md:text-pc-body-md">
             {description}
         </span>
     )
