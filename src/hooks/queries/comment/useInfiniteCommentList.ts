b06ec940fe1313eb043e480b19494791fc21c19a
Kim-HyounSuk <kkoor586@naver.com>
2025-05-16 23:38:37 +0900

    ✨ 댓글 관련 커스텀 쿼리 훅 작성

---
+++ src/hooks/queries/comment/useInfiniteCommentList.ts
@@ -0,0 +1,25 @@
+import { GcTime } from '@/constants/common/gcTime'
+import { QueryKey } from '@/constants/common/queryKey'
+import { StaleTime } from '@/constants/common/staleTime'
+import { getCommentList } from '@/lib/api/comment'
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
+
+export const useInfiniteCommentList = (quizId: string) =>
+    useSuspenseInfiniteQuery({
+        queryKey: [QueryKey.comment.LIST, quizId],
+        queryFn: ({ pageParam }) =>
+            getCommentList(quizId, {
+                cursor: pageParam ?? undefined,
+            }),
+        getNextPageParam: (nextPage) =>
+            nextPage.nextCursor
+                ? JSON.stringify(nextPage.nextCursor)
+                : undefined,
+        staleTime: StaleTime.MINUTE * 5, // (5분)
+        gcTime: GcTime.MINUTE * 5, // (5분)
+        retry: 1,
+        meta: {
+            ignoreGlobalError: true,
+        },
+        initialPageParam: undefined as string | undefined,
+    })
---
+++ src/hooks/queries/comment/useInfiniteRecommentList.ts
@@ -0,0 +1,25 @@
+import { GcTime } from '@/constants/common/gcTime'
+import { QueryKey } from '@/constants/common/queryKey'
+import { StaleTime } from '@/constants/common/staleTime'
+import { getRecommentList } from '@/lib/api/comment/getRecommentList'
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
+
+export const useInfiniteRecommentList = (commentId: string) =>
+    useSuspenseInfiniteQuery({
+        queryKey: [QueryKey.comment.RECOMMENT_LIST, commentId],
+        queryFn: ({ pageParam }) =>
+            getRecommentList(commentId, {
+                cursor: pageParam ?? undefined,
+            }),
+        getNextPageParam: (nextPage) =>
+            nextPage.nextCursor
+                ? JSON.stringify(nextPage.nextCursor)
+                : undefined,
+        staleTime: StaleTime.MINUTE * 5, // (5분)
+        gcTime: GcTime.MINUTE * 5, // (5분)
+        retry: 1,
+        meta: {
+            ignoreGlobalError: true,
+        },
+        initialPageParam: undefined as string | undefined,
+    })
