d89e333d489f36712c906eecdfd625b3557a1b72
Pa55er <tjrwns2715@naver.com>
2025-05-20 01:04:20 +0900

    ✨ 리뷰 생성하는 mutation 작성

---
+++ src/hooks/mutations/review/usePostReview.ts
@@ -0,0 +1,28 @@
+import { postReview } from '@/lib/api/review'
+import { CreateReview } from '@/types/review'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+
+/**
+ * 리뷰를 생성하는 mutation
+ */
+export const usePostReview = (quizbookId: string) => {
+    const queryClient = useQueryClient()
+    const router = useRouter()
+
+    return useMutation({
+        mutationFn: (data: CreateReview) => postReview(data),
+        retry: 0,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['quizbook-reviews', quizbookId],
+            })
+            queryClient.invalidateQueries({
+                queryKey: ['quizbook-states', quizbookId],
+            })
+            router.push(`/quizbook/${quizbookId}/info`)
+            toast('그룹에 추가되었습니다!')
+        },
+    })
+}
