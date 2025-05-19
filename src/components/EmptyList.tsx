9b76b8f709e1116fc9e62cd19edc8d73f09c615c
ppyom <ouob77@kakao.com>
2025-05-19 14:06:33 +0900

    🩹 EmptyList에 text 추가

--- src/components/EmptyList.tsx
+++ src/components/EmptyList.tsx
@@ -1,13 +1,14 @@
 interface Props {
     Icon: React.FC<{ className?: string }>
+    text?: string
 }
 
-export default function EmptyList({ Icon }: Props) {
+export default function EmptyList({ Icon, text }: Props) {
     return (
         <div className="flex select-none flex-col items-center gap-4 py-12">
             <Icon className="size-24 text-gray-300 md:size-32" />
             <p className="text-mobile-body-lg text-gray-400 md:text-pc-body-lg">
-                검색 결과가 존재하지 않습니다.
+                {text || '검색 결과가 존재하지 않습니다.'}
             </p>
         </div>
     )
