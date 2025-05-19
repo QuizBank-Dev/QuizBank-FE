3bf097e832dac3b67af0b89bb8ad2b1b1af3b16f
Pa55er <tjrwns2715@naver.com>
2025-05-20 01:28:57 +0900

    ✨ 리뷰 생성 모달 api 연동

--- src/app/quizbook/(no-layout)/[quizbookId]/(layout)/info/@modal/[modalType]/_components/CreateReviewModal.tsx
+++ src/app/quizbook/(no-layout)/[quizbookId]/(layout)/info/@modal/[modalType]/_components/CreateReviewModal.tsx
@@ -7,22 +7,20 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import clsx from 'clsx'
 import StarScore from './StarScore'
 import { ReviewFormData, reviewSchema } from '@/types/schemas/review'
+import { useParams } from 'next/navigation'
+import { usePostReview } from '@/hooks/mutations/review'
 
 export default function CreateReviewModal() {
-    const [isLoading, setIsLoading] = useState(false)
+    const { quizbookId } = useParams()
     const [score, setScore] = useState(5)
     const methods = useForm<ReviewFormData>({
         resolver: zodResolver(reviewSchema),
         mode: 'onChange',
     })
+    const { mutate, isPending } = usePostReview(quizbookId as string)
 
     const handleFormSubmit = async (data: ReviewFormData) => {
-        // 추후 로직 수정
-        setIsLoading(true)
-        setTimeout(() => {
-            console.log('Form Data:', { ...data, score })
-            setIsLoading(false)
-        }, 2000)
+        mutate({ ...data, score, quizbookId: quizbookId as string })
     }
 
     return (
@@ -38,19 +36,19 @@ export default function CreateReviewModal() {
                         name="content"
                         area={true}
                         placeholder="후기를 작성해보세요!"
-                        disabled={isLoading}
+                        disabled={isPending}
                     />
                     <div className="flex w-full justify-end">
                         <button
                             type="submit"
-                            disabled={isLoading}
+                            disabled={isPending}
                             className={clsx(
                                 'btn-solid btn-mobile-lg md:btn-pc-lg',
-                                isLoading && 'btn-loading',
+                                isPending && 'btn-loading',
                             )}
                         >
-                            {isLoading && <LoopAnimation />}
-                            {isLoading ? 'Loading...' : '제출'}
+                            {isPending && <LoopAnimation />}
+                            {isPending ? 'Loading...' : '제출'}
                         </button>
                     </div>
                 </form>
--- src/types/schemas/review/post-review.schema.ts
+++ src/types/schemas/review/post-review.schema.ts
@@ -1,7 +1,10 @@
 import * as z from 'zod'
 
 export const reviewSchema = z.object({
-    content: z.string().max(50, { message: '리뷰는 50자 이하로 해주세요' }),
+    content: z
+        .string()
+        .min(1, { message: '리뷰 내용은 꼭 입력해주세요' })
+        .max(50, { message: '리뷰는 50자 이하로 해주세요' }),
 })
 
 export type ReviewFormData = z.infer<typeof reviewSchema>
