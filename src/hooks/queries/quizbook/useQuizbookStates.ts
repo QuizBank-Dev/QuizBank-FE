3fa1cd4d7a7abbc25aeb55bd66fa58ae2ed72746
Pa55er <tjrwns2715@naver.com>
2025-05-19 13:58:45 +0900

    ✨ 특정 Quizbook의 통계 정보를 가져오는 query 작성

---
+++ src/hooks/queries/quizbook/useQuizbookStates.ts
@@ -0,0 +1,17 @@
+import { GcTime } from '@/constants/common/gcTime'
+import { StaleTime } from '@/constants/common/staleTime'
+import { getQuizbookStates } from '@/lib/api/quizbook'
+import { useQuery } from '@tanstack/react-query'
+
+/**
+ * 특정 Quizbook의 통계 정보를 가져오는 query
+ */
+export const useQuizbookStates = (quizbookId: string) => {
+    return useQuery({
+        queryKey: ['quizbook', quizbookId],
+        queryFn: () => getQuizbookStates(quizbookId),
+        staleTime: StaleTime.DEFAULT,
+        gcTime: GcTime.DEFAULT,
+        retry: 0,
+    })
+}
