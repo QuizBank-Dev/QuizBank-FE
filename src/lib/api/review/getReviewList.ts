67080ab8c024788440f2127e7d2ce9cb549c2a8f
Pa55er <tjrwns2715@naver.com>
2025-05-19 20:49:12 +0900

    ✨ 문제집의 리뷰 리스트를 조회 요청하는 함수 작성

---
+++ src/lib/api/review/getReviewList.ts
@@ -0,0 +1,25 @@
+import { GroupList } from '@/types/group'
+import axiosInstance from '../base'
+import { Response } from '@/types/base'
+
+/**
+ * 문제집의 리뷰 리스트를 조회 요청하는 함수
+ * @param quizbookId 문제집의 ID
+ * @param limit 불러올 항목 개수
+ * @param cursor 기준이 되는 리뷰의 ID
+ */
+export const getReviewList = async (
+    quizbookId: string,
+    limit: number = 3,
+    cursor?: string,
+) => {
+    const params = new URLSearchParams()
+    if (cursor) params.append('cursor', cursor)
+    params.append('limit', limit.toString())
+    params.append('quizbookId', quizbookId)
+
+    const res = await axiosInstance.get<Response<GroupList>>(
+        `/v1/review/quizbook/${quizbookId}?${params.toString()}`,
+    )
+    return res.data.result
+}
