9e289eebbc35ff3a91611a87da0c7740f3f5ecb2
Pa55er <tjrwns2715@naver.com>
2025-05-19 21:21:54 +0900

    ✨ 문제집 리뷰 리스트를 조회하는 query 작성

---
+++ src/hooks/queries/review/useReviewListQuery.ts
@@ -0,0 +1,18 @@
+import { GcTime } from '@/constants/common/gcTime'
+import { getReviewList } from '@/lib/api/review'
+import { useInfiniteQuery } from '@tanstack/react-query'
+
+/**
+ *  문제집 리뷰 리스트를 조회하는 query
+ */
+export const useReviewListQuery = (quizbookId: string, limit: number = 3) => {
+    return useInfiniteQuery({
+        queryKey: ['quizbook-reviews', quizbookId],
+        queryFn: ({ pageParam }) => getReviewList(quizbookId, limit, pageParam),
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+        initialPageParam: '',
+        staleTime: 0,
+        gcTime: GcTime.DEFAULT,
+        retry: 0,
+    })
+}
