beeedd38420347bfe2957391e8361771b0152883
Kim-HyounSuk <kkoor586@naver.com>
2025-05-20 00:21:51 +0900

    ✨ 무한 스크롤을 위한 trigger 커스텀 훅 작성

---
+++ src/hooks/useInfiniteScrollTrigger.ts
@@ -0,0 +1,50 @@
+import { useEffect, useRef } from 'react'
+
+interface Props {
+    rootRef: React.RefObject<HTMLDivElement | null>
+    fetchNextPage: () => void
+    hasNextPage: boolean
+    isFetchingNextPage: boolean
+}
+
+export const useInfiniteScrollTrigger = ({
+    rootRef,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+}: Props) => {
+    const triggerRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        if (
+            !triggerRef.current ||
+            !rootRef?.current ||
+            !hasNextPage ||
+            isFetchingNextPage
+        )
+            return
+
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (
+                    entry.isIntersecting &&
+                    hasNextPage &&
+                    !isFetchingNextPage
+                ) {
+                    console.log('트리거 동작')
+                    fetchNextPage()
+                }
+            },
+            {
+                root: rootRef.current,
+                threshold: 1.0,
+            },
+        )
+
+        observer.observe(triggerRef.current)
+
+        return () => observer.disconnect()
+    }, [rootRef, fetchNextPage, hasNextPage, isFetchingNextPage])
+
+    return triggerRef
+}
