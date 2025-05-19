88bb410890e6ed0690b2dd477822b955ffe81aa2
Kim-HyounSuk <kkoor586@naver.com>
2025-05-20 00:20:34 +0900

    🐛 타입 오류 수정

--- src/lib/api/comment/getCommentList.ts
+++ src/lib/api/comment/getCommentList.ts
@@ -1,13 +1,14 @@
 import { PaginationParams } from '@/types/api/base'
 import axiosInstance from '../base'
 import { PaginationResponse } from '@/types/base'
+import { Comment } from '@/types/comment'
 
 export const getCommentList = async (
     quizId: string,
-    params: PaginationParams,
+    params?: PaginationParams,
 ) => {
     const res = await axiosInstance.get<PaginationResponse<Comment[]>>(
-        `v1/comment/quiz/${quizId}`,
+        `/v1/comment/quiz/${quizId}`,
         {
             params,
         },
