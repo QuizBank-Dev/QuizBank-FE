98f8a7c04409f3f45734666100b8b651e49d4030
Kim-HyounSuk <kkoor586@naver.com>
2025-05-20 23:37:47 +0900

    ♻️ toast 메시지 추가

--- src/app/quizbook/(no-layout)/[quizbookId]/(no-layout)/result/_components/BookmarkBtn.tsx
+++ src/app/quizbook/(no-layout)/[quizbookId]/(no-layout)/result/_components/BookmarkBtn.tsx
@@ -5,6 +5,7 @@ import BookMarkSvg from '@/assets/svgs/bookmark.svg'
 import { usePostQuizLike } from '@/hooks/mutations/like'
 import clsx from 'clsx'
 import { useState } from 'react'
+import { toast } from 'sonner'
 
 interface Props {
     quizId: string
@@ -22,6 +23,10 @@ export default function BookmarkBtn({ quizId, initState }: Props) {
         mutate(undefined, {
             onSuccess: (data) => {
                 setIsLiked(data.state)
+                toast.success(`북마크 ${data.state ? '추가' : '제거'} 됨.`)
+            },
+            onError: () => {
+                toast.error('북마크 등록 중 에러가 발생했습니다.')
             },
         })
     }
