fc36b1c1caafc20f5d205f50ebf3403ed71f5c07
Pa55er <tjrwns2715@naver.com>
2025-05-24 18:00:24 +0900

    ✨ 개발자 정보 상수

---
+++ src/constants/developers.ts
@@ -0,0 +1,5 @@
+export const developers = [
+    { nickname: 'Vanlan', link: 'https://github.com/Kim-HyounSuk' },
+    { nickname: 'Pa55er', link: 'https://github.com/Pa55er' },
+    { nickname: 'ppyom', link: 'https://github.com/ppyom' },
+]
