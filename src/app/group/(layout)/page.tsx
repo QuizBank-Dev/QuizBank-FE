2b0e1bf4cd5e4245f5f14fa5b0db945364023249
Pa55er <tjrwns2715@naver.com>
2025-05-24 19:48:19 +0900

    ✨ 그룹 검색 페이지에 공통 footer 컴포넌트 적용

--- src/app/group/(layout)/page.tsx
+++ src/app/group/(layout)/page.tsx
@@ -1,5 +1,6 @@
 import MobileHeader from '@/components/MobileHeader'
 import GroupSearch from './_components/GroupSearch'
+import DesktopFooter from '@/components/DesktopFooter'
 
 export default function GroupPage() {
     return (
@@ -10,14 +11,17 @@ export default function GroupPage() {
             </MobileHeader>
 
             {/* 컨탠츠 */}
-            <main className="no-scrollbar flex w-full flex-1 flex-col items-center overflow-auto">
-                <div className="flex w-full max-w-[1056px] flex-col gap-4 px-4 py-4 md:py-8">
+            <div className="no-scrollbar flex h-full w-full flex-1 flex-col items-center justify-between overflow-auto">
+                <main className="flex w-full max-w-[1056px] flex-col gap-4 px-4 py-4 md:py-8">
                     <h2 className="hidden text-pc-title-sm font-extra-bold text-point-900 md:block">
                         그룹 검색
                     </h2>
                     <GroupSearch />
-                </div>
-            </main>
+                </main>
+
+                {/* 데스크탑 푸터 */}
+                <DesktopFooter />
+            </div>
         </>
     )
 }
