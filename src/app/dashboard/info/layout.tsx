00ece4d5391a811c4d52d9e53eaca84459d014d2
Kim-HyounSuk <kkoor586@naver.com>
2025-05-26 08:45:30 +0900

    💄 학습현황 레이아웃 수정

--- src/app/dashboard/info/layout.tsx
+++ src/app/dashboard/info/layout.tsx
@@ -11,12 +11,14 @@ export default function Layout({ children }: Props) {
             <MobileHeader backBtn={true} title="학습 현황">
                 <MobileHeader.UserMenu />
             </MobileHeader>
-            <div className="flex w-full max-w-[1024px] flex-1 flex-col overflow-y-auto md:mx-auto md:flex-row">
-                <main className="flex min-w-0 flex-col p-[16px] pb-[8px] md:order-2 md:flex-1 md:pb-[16px]">
-                    {children}
-                </main>
-                <div className="p-[16px] pt-[8px] md:pt-[16px]">
-                    <DashboardSidebar />
+            <div className="flex min-h-0 flex-1 flex-col overflow-y-auto">
+                <div className="flex w-full max-w-[1024px] flex-1 flex-col md:mx-auto md:flex-row">
+                    <main className="flex min-w-0 flex-col p-[16px] pb-[8px] md:order-2 md:flex-1 md:pb-[16px]">
+                        {children}
+                    </main>
+                    <div className="p-[16px] pt-[8px] md:pt-[16px]">
+                        <DashboardSidebar />
+                    </div>
                 </div>
             </div>
         </>
--- src/app/dashboard/info/page.tsx
+++ src/app/dashboard/info/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <section className="flex flex-col gap-[16px]">
             <h2 className="hidden text-mobile-title-sm font-extra-bold text-point-900 md:block md:text-pc-title-sm">
-                학습현황
+                학습 현황
             </h2>
             <div className="flex flex-col gap-[16px]">
                 <div className="flex flex-col gap-[16px] lg:flex-row">
