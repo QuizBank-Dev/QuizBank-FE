374f0efc3aadddd19de00c3d5ade48e4b01eb8d2
ppyom <ouob77@kakao.com>
2025-05-26 13:55:05 +0900

    ✨ 비밀번호 재설정 API 연동

--- src/app/(auth)/reset-password/_components/ResetForm.tsx
+++ src/app/(auth)/reset-password/_components/ResetForm.tsx
@@ -1,29 +1,50 @@
 'use client'
 
+import { useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { FormProvider, useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { AxiosError } from 'axios'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { CustomInput } from '@/components'
 import {
     ResetPasswordFormData,
     resetPasswordSchema,
 } from '@/types/schemas/auth'
+import { EmptyResponse } from '@/types/base'
+import { confirmResetPassword } from '@/lib/api/auth'
+import LoadingButton from '@/app/(auth)/_components/LoadingButton'
 
 export default function ResetForm() {
     const router = useRouter()
     const searchParams = useSearchParams()
+    const [isLoading, setIsLoading] = useState(false)
 
     const methods = useForm<ResetPasswordFormData>({
         resolver: zodResolver(resetPasswordSchema),
         mode: 'onChange',
     })
 
-    const handleFormSubmit = async ({ newPassword }: ResetPasswordFormData) => {
+    const handleFormSubmit = async (formData: ResetPasswordFormData) => {
         const token = searchParams.get('token')
 
-        // API 호출
-        console.log(token, newPassword)
-        router.push('/login')
+        setIsLoading(true)
+        confirmResetPassword(token || '', formData)
+            .then(() => {
+                router.push('/login')
+                toast('비밀번호가 재설정되었습니다.')
+            })
+            .catch((error: AxiosError<EmptyResponse>) => {
+                const data = error.response?.data
+                toast(
+                    data
+                        ? data.message
+                        : '비밀번호 재설정 중 오류가 발생했습니다.',
+                )
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
     }
 
     return (
@@ -48,12 +69,14 @@ export default function ResetForm() {
                     placeholder="비밀번호를 다시 입력해주세요"
                     style="solid"
                 />
-                <button
+                <LoadingButton
                     type="submit"
-                    className="btn-solid btn-mobile-lg md:btn-pc-lg"
+                    size="lg"
+                    isLoading={isLoading}
+                    loadingMessage="Loading..."
                 >
                     비밀번호 재설정
-                </button>
+                </LoadingButton>
             </form>
         </FormProvider>
     )
