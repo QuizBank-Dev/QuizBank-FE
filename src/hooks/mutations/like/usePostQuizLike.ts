2a5711a25c3d32461e7fa3fe225f1ac5958d4e25
Kim-HyounSuk <kkoor586@naver.com>
2025-05-26 12:55:55 +0900

    ♻️ Quiz 북마크 요청 성공 시 다시볼문제 쿼리 캐시 무효화 추가

--- src/hooks/mutations/like/usePostQuizLike.ts
+++ src/hooks/mutations/like/usePostQuizLike.ts
@@ -1,11 +1,20 @@
+import { QueryKey } from '@/constants/common/queryKey'
 import { postQuizLike } from '@/lib/api/like'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
-export const usePostQuizLike = (quizId: string) =>
-    useMutation({
+export const usePostQuizLike = (quizId: string) => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
         mutationFn: () => postQuizLike(quizId),
         retry: 0,
         meta: {
             ignoreGlobalError: true,
         },
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: QueryKey.like.QUIZ_LIST,
+            })
+        },
     })
+}
