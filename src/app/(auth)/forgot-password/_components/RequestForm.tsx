525c71d7a4886d8bfb6ccc516855280f22f50337
ppyom <ouob77@kakao.com>
2025-05-26 13:46:50 +0900

    ✨ 비밀번호 재설정 요청 API 연동

--- src/app/(auth)/forgot-password/_components/RequestForm.tsx
+++ src/app/(auth)/forgot-password/_components/RequestForm.tsx
@@ -1,26 +1,34 @@
 'use client'
 
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { FormProvider, useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { CustomInput } from '@/components'
 import {
     ForgotPasswordFormData,
     forgotPasswordSchema,
 } from '@/types/schemas/auth'
+import { requestResetPassword } from '@/lib/api/auth'
+import LoadingButton from '../../_components/LoadingButton'
 
 export default function RequestForm() {
     const router = useRouter()
+    const [isLoading, setIsLoading] = useState(false)
 
     const methods = useForm<ForgotPasswordFormData>({
         resolver: zodResolver(forgotPasswordSchema),
         mode: 'onChange',
     })
 
-    const handleFormSubmit = async ({ email }: ForgotPasswordFormData) => {
-        // API 호출
-        console.log(email)
-        router.push('/login')
+    const handleFormSubmit = async (formData: ForgotPasswordFormData) => {
+        setIsLoading(true)
+        requestResetPassword(formData).finally(() => {
+            toast('비밀번호 재설정 안내 메일이 전송되었습니다.')
+            router.push('/login')
+            setIsLoading(false)
+        })
     }
 
     return (
@@ -36,12 +44,14 @@ export default function RequestForm() {
                     placeholder="이메일 주소를 입력해주세요"
                     style="solid"
                 />
-                <button
+                <LoadingButton
                     type="submit"
-                    className="btn-solid btn-mobile-lg md:btn-pc-lg"
+                    size="lg"
+                    isLoading={isLoading}
+                    loadingMessage="Loading..."
                 >
                     비밀번호 재설정 메일 전송
-                </button>
+                </LoadingButton>
             </form>
         </FormProvider>
     )
---
+++ src/lib/api/auth/password.ts
@@ -0,0 +1,37 @@
+import axiosInstance from '@/lib/api/base'
+import {
+    ForgotPasswordFormData,
+    ResetPasswordFormData,
+} from '@/types/schemas/auth'
+import { EmptyResponse } from '@/types/base'
+
+/**
+ * 비밀번호 재설정 메일을 요청하는 함수
+ * @param email 비밀번호를 재설정할 이메일 주소
+ */
+export const requestResetPassword = async ({
+    email,
+}: ForgotPasswordFormData) => {
+    return await axiosInstance.post<EmptyResponse>(
+        'v1/auth/reset-password/request',
+        { email },
+    )
+}
+
+/**
+ * 비밀번호를 재설정하는 함수
+ * @param token 발급받은 토큰
+ * @param newPassword 변경할 비밀번호
+ */
+export const confirmResetPassword = async (
+    token: string,
+    { newPassword }: ResetPasswordFormData,
+) => {
+    return await axiosInstance.post<EmptyResponse>(
+        'v1/auth/reset-password/confirm',
+        {
+            token,
+            newPassword,
+        },
+    )
+}
