ea4cff9d54e695e6ddc9f4a055a69ce265f8a3fc
ppyom <ouob77@kakao.com>
2025-05-29 13:37:22 +0900

    🩹 최대 넓이를 다른 페이지와 동일하게 수정 / 간격 수정

--- src/app/(needsCategories)/(home)/page.tsx
+++ src/app/(needsCategories)/(home)/page.tsx
@@ -4,7 +4,7 @@ import RecommendedQuizbookList from './_components/RecommendedQuizbookList'
 
 export default function Home() {
     return (
-        <main className="flex w-full max-w-[1024px] flex-col gap-4 p-4 pb-8">
+        <main className="flex w-full max-w-[1056px] flex-col gap-4 p-4 pb-8">
             <MainBanner />
             <StudyDashboard />
             <RecommendedQuizbookList />
