21a5a2b92983c726c19bfc7c953acd53131d3a88
ppyom <ouob77@kakao.com>
2025-05-29 13:33:22 +0900

    💄 학습현황 컴포넌트 로그인을 하지 않은 상태일 때 디자인 수정

--- src/app/(needsCategories)/(home)/_components/StudyDashboard/index.tsx
+++ src/app/(needsCategories)/(home)/_components/StudyDashboard/index.tsx
@@ -12,12 +12,16 @@ export default function StudyDashboard() {
     const { data: user } = useCurrentUser()
     return (
         <section className="flex flex-col gap-[16px] pb-4">
-            <TitleWithMore title="학습 현황" link="/dashboard" loginRequired />
+            <TitleWithMore
+                title="나의 학습 현황"
+                link="/dashboard"
+                loginRequired
+            />
             <div className="relative">
                 <div
                     className={clsx(
                         'flex flex-col gap-[16px] lg:flex-row',
-                        !user && 'blur',
+                        !user && 'select-none blur',
                     )}
                 >
                     <BadgeBoard className="lg:order-2" />
@@ -27,17 +31,19 @@ export default function StudyDashboard() {
                     </div>
                 </div>
                 {!user && (
-                    <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-4 text-center">
-                        <p className="text-mobile-body-lg md:text-pc-body-lg">
-                            로그인하고 나의 학습 현황을 확인해보세요!
-                        </p>
-                        <Link
-                            href="/login"
-                            className="btn-solid btn-mobile-lg flex items-center gap-2 md:btn-pc-lg"
-                        >
-                            <LoginSvg className="size-5" />
-                            <span>로그인하기</span>
-                        </Link>
+                    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                        <div className="flex min-w-[300px] flex-col items-center gap-4 rounded-lg bg-white/80 p-6 shadow-xl">
+                            <p className="text-mobile-body-lg drop-shadow-[0_0_8px_#ffffff] md:text-pc-body-lg">
+                                로그인하고 나의 학습 현황을 확인해보세요!
+                            </p>
+                            <Link
+                                href="/login"
+                                className="btn-solid btn-mobile-lg flex items-center gap-2 md:btn-pc-lg"
+                            >
+                                <LoginSvg className="size-5" />
+                                <span>로그인하기</span>
+                            </Link>
+                        </div>
                     </div>
                 )}
             </div>
