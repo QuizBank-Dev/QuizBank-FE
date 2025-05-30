fccc6b877efd1b536ddc17a094d161cb647aa4d9
ppyom <ouob77@kakao.com>
2025-05-30 14:51:39 +0900

    🩹 변수명 수정

--- src/app/my-page/info/@modal/badge/_components/BadgeList/index.tsx
+++ src/app/my-page/info/@modal/badge/_components/BadgeList/index.tsx
@@ -9,20 +9,20 @@ import BadgeSvg from '@/assets/svgs/badge.svg'
 export default function BadgeList() {
     const { data: user } = useCurrentUser()
 
-    const badges = getBadgesStatus(user?.experience || 0)
-    const unlockBadges = badges.filter((badge) => badge.unlocked)
+    const badgeList = getBadgesStatus(user?.experience || 0)
+    const unlockBadgeList = badgeList.filter((badge) => badge.unlocked)
 
     return (
         <div className="flex flex-col">
-            {unlockBadges.length === 0 && (
+            {unlockBadgeList.length === 0 && (
                 <EmptyList Icon={BadgeSvg} text="보유한 뱃지가 없습니다." />
             )}
-            {unlockBadges.length !== 0 && (
+            {unlockBadgeList.length !== 0 && (
                 <>
-                    {unlockBadges.map((badge) => (
+                    {unlockBadgeList.map((badge) => (
                         <BadgeItem key={badge.id} {...badge} />
                     ))}
-                    {badges.length !== unlockBadges.length && (
+                    {badgeList.length !== unlockBadgeList.length && (
                         <p className="mt-2 self-center rounded-lg bg-point-50/50 px-6 py-2 text-center text-mobile-body-sm font-semi-bold text-gray-600 md:mt-4 md:py-4 md:text-pc-body-sm">
                             문제를 해결하며 성장하고, 더 많은 뱃지를
                             획득해보세요!
--- src/app/user/[userId]/_components/Profile/BadgeList.tsx
+++ src/app/user/[userId]/_components/Profile/BadgeList.tsx
@@ -6,17 +6,17 @@ interface Props {
 }
 
 export default function BadgeList({ experience }: Props) {
-    const badges = getBadgesStatus(experience || 0)
-    const unlockBadges = badges.filter((badge) => badge.unlocked)
+    const badgeList = getBadgesStatus(experience || 0)
+    const unlockBadgeList = badgeList.filter((badge) => badge.unlocked)
 
     return (
         <div className="flex items-center justify-center gap-1">
-            {unlockBadges.length === 0 && (
+            {unlockBadgeList.length === 0 && (
                 <p className="select-none py-2 text-mobile-body-sm text-gray-300 md:text-pc-body-sm">
                     뱃지가 존재하지 않습니다.
                 </p>
             )}
-            {unlockBadges.slice(-4).map(({ id, imageUrl, label }) => (
+            {unlockBadgeList.slice(-4).map(({ id, imageUrl, label }) => (
                 <ProfileImage
                     key={id}
                     size={48}
