fe223d807285c0c6f0eb1b6f151a809f8f10d9d5
Kim-HyounSuk <kkoor586@naver.com>
2025-06-02 02:55:09 +0900

    ♻️ icons 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,26 @@ const pretendard = localFont({
 })
 
 export const metadata: Metadata = {
-    title: 'Quizbank | AI 채점 및 문제 검증 기반 오픈 퀴즈 플랫폼',
+    title: 'Quizbank: AI 채점 및 문제 검증 기반 오픈 퀴즈 플랫폼',
     description:
         'AI가 문제를 자동으로 검토하고 채점해주는 새로운 학습 플랫폼. 누구나 직접 문제를 만들고 풀며 지식을 공유하고 성장할 수 있습니다.',
+    icons: {
+        icon: [
+            {
+                url: '/icons/favicon-32x32.png',
+                sizes: '32x32',
+                type: 'image/png',
+            },
+            {
+                url: '/icons/favicon-16x16.png',
+                sizes: '16x16',
+                type: 'image/png',
+            },
+            { url: '/icons/favicon.ico', rel: 'icon' },
+        ],
+        apple: { url: '/icons/apple-touch-icon.png', sizes: '180x180' },
+    },
+    manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
